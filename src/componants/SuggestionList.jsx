cabbfe36989c0f22776a18bc397c302e20df2ac7
dullat <dullat@users.noreply.github.com>
2024-07-09 09:56:17 +0530

    base: working

--- src/componants/SuggestionList.jsx
+++ src/componants/SuggestionList.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const SuggestionList = ({ suggestions }) => {
-  return <h1>hi</h1>;
+const SuggestionList = ({ suggestions = [] }) => {
+  return (
+    <React.Fragment>
+      {suggestions.map((suggestion, index) => {
+        const currSuggestion = suggestion.name;
+        return (
+          <li
+            key={index}
+            className="suggestion-item"
+            id={`suggestion-${index}`}
+          >
+            {currSuggestion}
+          </li>
+        );
+      })}
+    </React.Fragment>
+  );
 };
 
 export default SuggestionList;
